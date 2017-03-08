fb7e811853adceb84b1681ea8b90e924f6597f72
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-03-08 18:11:14 +0000

    Proper free

--- src/Control/Monad/Constrained/Free.hs
+++ src/Control/Monad/Constrained/Free.hs
@@ -3,36 +3,26 @@
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE TypeOperators    #-}
-
+{-# LANGUAGE ApplicativeDo    #-}
 module Control.Monad.Constrained.Free where
 
 import Control.Monad.Constrained hiding ((<$>), (<*>), pure, fmap)
 import qualified Control.Monad.Constrained as Constrained
 
-infixl 5 :>|
-data FreeAppVect :: (* -> *) -> [*] -> * where
-        NilAppl :: FreeAppVect f '[]
-        (:>|) ::
-            Suitable f x =>
-            FreeAppVect f xs -> App f x -> FreeAppVect f (x ': xs)
-
 data App :: (* -> *) -> * -> * where
         Pure :: a -> App f a
-        Appl :: FunType xs a -> FreeAppVect f xs -> App f a
-        Lift :: Suitable f a => f a -> App f a
+        Appl :: FunType xs a -> AppVect f xs -> App f a
 
 infixl 4 <*>
-(<*>) :: Suitable f a => App f (a -> b) -> App f a -> App f b
-Pure f <*> x = Appl f (NilAppl :>| x)
-Appl fs xs <*> ys = Appl fs (xs :>| ys)
-Lift f <*> x = Appl ($) (NilAppl :>| Lift f :>| x)
+(<*>) :: App f (a -> b) -> f a -> App f b
+Pure f <*> x = Appl f (Nil :> x)
+Appl fs xs <*> ys = Appl fs (xs :> ys)
 
-infixl 4 <$>
-(<$>) :: Suitable f a => (a -> b) -> App f a -> App f b
-f <$> x = Pure f <*> x
+fmap :: (a -> b) -> f a -> App f b
+fmap f x = Appl f (Nil :> x)
 
-fmap :: Suitable f a => (a -> b) -> App f a -> App f b
-fmap = (<$>)
+(<$>) :: (a -> b) -> f a -> App f b
+(<$>) = fmap
 
 pure :: a -> App f a
 pure = Pure
@@ -44,11 +34,16 @@ lower
     :: (Applicative f, Suitable f a)
     => App f a -> f a
 lower (Pure x) = Constrained.pure x
-lower (Lift x) = x
 lower (Appl fs xs) = liftA fs (lowerVect xs)
   where
     lowerVect
         :: Applicative f
-        => FreeAppVect f xs -> AppVect f xs
-    lowerVect NilAppl = Nil
-    lowerVect (ys :>| y) = lowerVect ys :> lower y
+        => AppVect f xs -> AppVect f xs
+    lowerVect Nil = Nil
+    lowerVect (ys :> y) = lowerVect ys :> y
+
+example :: (Applicative f, Suitable f Integer) => App f Integer
+example = do
+  x <- Constrained.pure 1
+  y <- Constrained.pure 2
+  pure (x + y)
