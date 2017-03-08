8741e7223cae7716c8c3c4f209383ad882d27860
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-03-08 19:29:26 +0000

    Correct free

--- src/Control/Monad/Constrained/Free.hs
+++ src/Control/Monad/Constrained/Free.hs
@@ -3,28 +3,36 @@
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE TypeOperators    #-}
-{-# LANGUAGE ApplicativeDo    #-}
+
 module Control.Monad.Constrained.Free where
 
 import Control.Monad.Constrained hiding ((<$>), (<*>), pure, fmap)
 import qualified Control.Monad.Constrained as Constrained
 
+infixl 5 :>|
+data FreeAppVect :: (* -> *) -> [*] -> * where
+        NilAppl :: FreeAppVect f '[]
+        (:>|) ::
+            Suitable f x =>
+            FreeAppVect f xs -> App f x -> FreeAppVect f (x ': xs)
+
 data App :: (* -> *) -> * -> * where
         Pure :: a -> App f a
-        Appl :: FunType xs a -> AppVect f xs -> App f a
-        Lift :: f a -> App f a
+        Appl :: FunType xs a -> FreeAppVect f xs -> App f a
+        Lift :: Suitable f a => f a -> App f a
 
 infixl 4 <*>
-(<*>) :: App f (a -> b) -> f a -> App f b
-Pure f <*> x = Appl f (Nil :> x)
-Lift f <*> x = Appl ($) (Nil :> f :> x)
-Appl fs xs <*> ys = Appl fs (xs :> ys)
+(<*>) :: Suitable f a => App f (a -> b) -> App f a -> App f b
+Pure f <*> x = Appl f (NilAppl :>| x)
+Appl fs xs <*> ys = Appl fs (xs :>| ys)
+Lift f <*> x = Appl ($) (NilAppl :>| Lift f :>| x)
 
-fmap :: (a -> b) -> f a -> App f b
-fmap f x = Appl f (Nil :> x)
+infixl 4 <$>
+(<$>) :: Suitable f a => (a -> b) -> App f a -> App f b
+f <$> x = Pure f <*> x
 
-(<$>) :: (a -> b) -> f a -> App f b
-(<$>) = fmap
+fmap :: Suitable f a => (a -> b) -> App f a -> App f b
+fmap = (<$>)
 
 pure :: a -> App f a
 pure = Pure
@@ -32,12 +40,12 @@ pure = Pure
 lower
     :: (Applicative f, Suitable f a)
     => App f a -> f a
-lower (Lift x) = x
 lower (Pure x) = Constrained.pure x
+lower (Lift x) = x
 lower (Appl fs xs) = liftA fs (lowerVect xs)
   where
     lowerVect
         :: Applicative f
-        => AppVect f xs -> AppVect f xs
-    lowerVect Nil = Nil
-    lowerVect (ys :> y) = lowerVect ys :> y
+        => FreeAppVect f xs -> AppVect f xs
+    lowerVect NilAppl = Nil
+    lowerVect (ys :>| y) = lowerVect ys :> lower y
